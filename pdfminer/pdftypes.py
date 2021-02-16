804144411ec218bfd3b53f565ccce6ffbce52412
amitagari <amit.agari@naukri.com>
2021-02-16 20:08:40 +0530

    empty check for params

--- pdfminer/pdftypes.py
+++ pdfminer/pdftypes.py
@@ -270,7 +270,7 @@ class PDFStream(PDFObject):
             else:
                 raise PDFNotImplementedError('Unsupported filter: %r' % f)
             # apply predictors
-            if 'Predictor' in params:
+            if params is not None and  'Predictor' in params:
                 pred = int_value(params['Predictor'])
                 if pred == 1:
                     # no predictor
