8376b9097f5c326bde86b5c1d30fcd405d51f46c
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2022-09-01 10:58:04 +0200

    Refactored tests and fixed flake8 issues

--- tests/test_rijndael.py
+++ tests/test_rijndael.py
@@ -8,14 +8,16 @@ class TestArcFourMethods(unittest.TestCase):
         plaintext = bytes.fromhex('506812a45f08c889b97f5980038b8359')
         expected = 'd8f532538289ef7d06b506a4fd5be9c9'
 
-        self.assertEqual(RijndaelEncryptor(key, 128).encrypt(plaintext).hex(), expected)
+        self.assertEqual(RijndaelEncryptor(key, 128).encrypt(plaintext).hex(),
+                         expected)
 
     def test_decryptor(self):
         key = bytes.fromhex('00010203050607080a0b0c0d0f101112')
         ciphertext = bytes.fromhex('d8f532538289ef7d06b506a4fd5be9c9')
         expected = '506812a45f08c889b97f5980038b8359'
 
-        self.assertEqual(RijndaelDecryptor(key, 128).decrypt(ciphertext).hex(), expected)
+        self.assertEqual(RijndaelDecryptor(key, 128).decrypt(ciphertext).hex(),
+                         expected)
 
 
 if __name__ == '__main__':
