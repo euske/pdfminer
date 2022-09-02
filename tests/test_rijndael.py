b38f50f336f8b84d225705995f5a8469d8f5f696
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2022-09-02 09:12:02 +0200

    Fixed copy pasta error

--- tests/test_rijndael.py
+++ tests/test_rijndael.py
@@ -2,7 +2,7 @@ import unittest
 from pdfminer.rijndael import RijndaelEncryptor, RijndaelDecryptor
 
 
-class TestArcFourMethods(unittest.TestCase):
+class TestRijndael(unittest.TestCase):
     def test_encryptor(self):
         key = bytes.fromhex('00010203050607080a0b0c0d0f101112')
         plaintext = bytes.fromhex('506812a45f08c889b97f5980038b8359')
--- tests/test_runlength.py
+++ tests/test_runlength.py
@@ -2,7 +2,7 @@ import unittest
 from pdfminer.runlength import rldecode
 
 
-class TestArcFourMethods(unittest.TestCase):
+class TestRunLength(unittest.TestCase):
     def test_encryptor(self):
         string = b'\x05123456\xfa7\x04abcde\x80junk'
         expected = b'1234567777777abcde'
