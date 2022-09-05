dfed505489f7e99314857f89604641cb33cf8674
Gabriel Lindgren <gabriel.lindgren@bontour.com>
2022-09-05 12:09:26 +0200

    fixed flake8 errors for test_dumpoutline.py

--- tests/test_dumpoutline.py
+++ tests/test_dumpoutline.py
@@ -2,27 +2,45 @@ import unittest
 from tools.dumppdf import dumpoutline
 import sys
 
+
 class TestDumpOutline(unittest.TestCase):
 
-  def test_dumpoutline(self):
-    directory = 'samples/'
-    files = ['ex1.pdf', 'ex2.pdf', 'example.pdf', 'jo.pdf', 'sample.pdf', 'simple1.pdf', 'simple2.pdf', 'simple3.pdf']
-    objids = []
-    pagenos = set()
-    mode = None
-    password = b''
-    dumpall = False
-    outfp = sys.stdout
-    extractdir = None
+    def test_dumpoutline(self):
+        directory = 'samples/'
+        files = [
+            'ex1.pdf',
+            'ex2.pdf',
+            'example.pdf',
+            'jo.pdf',
+            'sample.pdf',
+            'simple1.pdf',
+            'simple2.pdf',
+            'simple3.pdf'
+        ]
+        objids = []
+        pagenos = set()
+        mode = None
+        password = b''
+        dumpall = False
+        outfp = sys.stdout
+        extractdir = None
 
-    for file in files:
-      result = dumpoutline(outfp, directory+file, objids, pagenos, password=password,
-              dumpall=dumpall, mode=mode, extractdir=extractdir)
+        for file in files:
+            result = dumpoutline(
+                outfp,
+                directory+file,
+                objids,
+                pagenos,
+                password=password,
+                dumpall=dumpall,
+                mode=mode,
+                extractdir=extractdir
+            )
 
-      notAllowed = '&<>"'
+        notAllowed = '&<>"'
 
-      self.assertFalse(set(notAllowed).intersection(result))
+        self.assertFalse(set(notAllowed).intersection(result))
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
