f53a7f628d0f74659960463f51effb2a6e2538e8
Gholam Mohammadi <gholam.mohammadi.2789@student.uu.se>
2022-09-06 09:54:34 +0200

    Fixed not module found error

--- tests/test_lzw.py
+++ tests/test_lzw.py
@@ -1,18 +1,12 @@
-import unittest2
-from lzw import lzwdecode
+import unittest
+from pdfminer.lzw import lzwdecode
 
-class TestLzwMethods(unittest2.TestCase):
+
+class TestLzwMethods(unittest.TestCase):
     def test1(self):
         obj = lzwdecode(bytes.fromhex('800b6050220c0c8501'))
         self.assertEqual(obj ,b'-----A---B')
 
 
 if __name__ == '__main__':
-    unittest2.main()
-
-
-
-"""
-    >>> lzwdecode(bytes.fromhex('800b6050220c0c8501'))
-    b'-----A---B'
-    """
+    unittest.main()
