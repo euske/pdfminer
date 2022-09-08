d5da2af0e17cd6317d759f6f40825cc8211aad16
Amer Slaiman <amer.m.sl.248@gmail.com>
2022-09-08 20:05:13 +0200

    add_documentation

--- tests/test_rijndael.py
+++ tests/test_rijndael.py
@@ -3,11 +3,15 @@ from pdfminer.rijndael import *
 
 
 class MyTestCase(unittest.TestCase):
+    # test for the decryption function
+
     def test_1(self):
         key = bytes.fromhex('00010203050607080a0b0c0d0f101112')
         ciphertext = bytes.fromhex('d8f532538289ef7d06b506a4fd5be9c9')
         k_1 = RijndaelDecryptor(key, 128).decrypt(ciphertext).hex()
         self.assertEqual(k_1, '506812a45f08c889b97f5980038b8359')
+
+    # test for the encryption function
 
     def test_2(self):
         key = bytes.fromhex('00010203050607080a0b0c0d0f101112')
