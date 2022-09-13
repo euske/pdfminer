4d33d3c22d80328137331f63a3ea675388051515
Johan.Lovgren <johan.lovgren.5795@student.uu.se>
2022-09-13 13:02:43 +0200

    Added first fix of png support and two pdf test files

--- pdfminer/image.py
+++ pdfminer/image.py
@@ -71,13 +71,11 @@ class ImageWriter:
 
     def export_image(self, image):
         stream = image.stream
-        filters = stream.get_filters()
+        filters = list(stream.get_filters())
         (width, height) = image.srcsize
         if len(filters) == 1 and filters[0][0] in LITERALS_DCT_DECODE:
             ext = '.jpg'
-        elif (image.bits == 1 or
-              image.bits == 8 and image.colorspace in
-              (LITERAL_DEVICE_RGB, LITERAL_DEVICE_GRAY)):
+        elif (image.bits == 1 or image.bits == 8 and image.colorspace[0] in (LITERAL_DEVICE_RGB, LITERAL_DEVICE_GRAY)):
             ext = '.%dx%d.bmp' % (width, height)
         else:
             ext = '.%d.%dx%d.img' % (image.bits, width, height)
@@ -96,6 +94,7 @@ class ImageWriter:
                     i.save(fp, 'JPEG')
                 else:
                     fp.write(raw_data)
+            # TODO add png here
             elif image.bits == 1:
                 bmp = BMPWriter(fp, 1, width, height)
                 data = stream.get_data()
@@ -104,7 +103,7 @@ class ImageWriter:
                 for y in range(height):
                     bmp.write_line(y, data[i:i+width])
                     i += width
-            elif image.bits == 8 and image.colorspace is LITERAL_DEVICE_RGB:
+            elif image.bits == 8 and image.colorspace[0] is LITERAL_DEVICE_RGB:
                 bmp = BMPWriter(fp, 24, width, height)
                 data = stream.get_data()
                 i = 0
@@ -112,7 +111,7 @@ class ImageWriter:
                 for y in range(height):
                     bmp.write_line(y, data[i:i+width])
                     i += width
-            elif image.bits == 8 and image.colorspace is LITERAL_DEVICE_GRAY:
+            elif image.bits == 8 and image.colorspace[0] is LITERAL_DEVICE_GRAY:
                 bmp = BMPWriter(fp, 8, width, height)
                 data = stream.get_data()
                 i = 0
