17865e32d1fd234dffec66b12cb0787960329569
barwepaul <abarwepaul@gmail.com>
2022-09-19 14:35:12 +0200

    line reader tests

--- tests/test_pdf2XmlConversion.py
+++ tests/test_pdf2XmlConversion.py
@@ -4,7 +4,7 @@ import tools.pdf2txt
 
 class TestWordprinting(unittest.TestCase):
 
-    def wordReader(self):
+    def testWordReader(self):
 
         argv = ['pdf2txt.py', '-o', 'samples/testxml.xml', '-t', 'xml',
                 'samples/XMLConverterTestFile.pdf']
@@ -29,3 +29,36 @@ class TestWordprinting(unittest.TestCase):
                     if '<text ' and '</text>' in line:
                         sub1 = line.split('bbox=')[1]
                         charCoordinateArray.append(sub1.split(' ')[0])
+
+
+class TestLineCoordinates(unittest.TestCase):
+
+    def testLineReader(self):
+
+        argv = ['pdf2txt.py', '-o', 'samples/testxml.xml', '-t', 'xml',
+                'samples/simple1.pdf'])
+        tools.pdf2txt.setOptionsAndConvert(argv)
+        with open('samples/test_outfp.txt', 'r') as f:
+            charCoordinateArray = []
+            lineCoordinate = ''
+            for line in f:
+                if '<textline>' in line:
+                    sub1 =  line.split('bbox=')[1]
+                    lineCoordinate = sub1.split(' ')[0]
+                elif '<text ' and '</text>' in line: 
+                    sub1 = line.split('bbox=')[1]
+                    charCoordinateArray.append(sub1.split(' ')[0])  
+                elif '</textline>' in line:
+                    firstString = charCoordinateArray[1]
+                    secondString = charCoordinateArray[
+                        len(charCoordinateArray)-1]
+                    firstCharCoordinates = firstString.split(',')
+                    secondCharCoordinates = secondString.split(',') 
+                    n = 2
+                    left = ','.join(firstCharCoordinates[:n])
+                    right = ','.join(secondCharCoordinates[n:])
+                    self.assertEqual(lineCoordinate, left+","+right)
+                    charCoordinateArray.clear   
+                 
+if __name__ == '__main__':
+    unittest.main()  
