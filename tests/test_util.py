7d06990c8c117769a7968981a5a6bbc64cc53e2a
Aske J L <aske.lorenz@gmail.com>
2022-09-19 14:53:09 +0200

    update util test

--- tests/test_util.py
+++ tests/test_util.py
@@ -6,10 +6,10 @@ class TestUtils(unittest.TestCase):
 
     def TestRightbbox2str(self):
         bbox = (25, 50, 30, 60)
-        rightbbox = '30,60'
+        rightbbox = '30.000,60.000'
         self.assertEqual(rightbbox, pdfminer.utils.rightbbox2str(bbox))
 
     def TestLeftbbox2str(self):
         bbox = (25, 50, 30, 60)
-        leftbbox = '25,50'
+        leftbbox = '25.000,50.000'
         self.assertEqual(leftbbox, pdfminer.utils.leftbbox2str(bbox))
