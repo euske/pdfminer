117f6235cae524fbf4635c089b76ec87ae7ed528
Erik Junghahn <erik.junghahn@gmail.com>
2022-09-20 21:34:13 +0200

    Commented chapterparser

--- pdfminer/chapterparser.py
+++ pdfminer/chapterparser.py
@@ -2,14 +2,31 @@
 import re
 
 
-class ChapterParser:
 
+
+class ChapterParser:
+    """
+    This function reads a textfile from a filepath.
+
+    @param inpfp: the filepath for the file to be read as a string.
+    @return: The text in the given file as a string.
+    
+    """
     def cp_read_text(self, inpfp):
         filehandle = open(inpfp)
         text = filehandle.read()
         filehandle.close()
         return text
 
+
+    """
+    This function splits a string into substrings where it finds 
+    some occurance of the text "Chapter" followed by a number
+
+    @param text: The string to be split into multiple substrings.
+    @return: A list containing multile substrings of chapters.
+    
+    """
     def cp_split(self, text):
         chapterList = re.split(
             'Chapter ' + r'\d+', text, flags=re.IGNORECASE)[1:]
@@ -19,6 +36,15 @@ class ChapterParser:
 
         return chapterList
 
+    """
+    This function writes the content of each value of the chapters list into
+    a unqiue file at the given output folder directory
+
+    @param chapters: A list containing multile substrings of chapters.
+    @param outpath: A string describing the directory of the generated txt files.
+    
+    
+    """
     def cp_write_chapters(self, chapters, outpath):
         for i, chapter in enumerate(chapters):
             outfp = outpath + 'chapter' + str(i+1) + '.txt'
@@ -27,6 +53,16 @@ class ChapterParser:
                 f.write(chapter)
         return
 
+
+    """
+    This auxilliary function performs the full chapter split operation by 
+    reading a textfile, splitting it and writing it anew.
+
+    @param inpfp: A string describing the directory of the file containing the text to split.
+    @param outpath: A string describing the directory of the generated txt files.
+    
+    
+    """
     def split_chapters(self, inpfp, outpath):
 
         text = ChapterParser.cp_read_text(self, inpfp)
