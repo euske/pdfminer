68bf80a00464e861edc56afb6ae79a28c40157af
Daniel Fehrm <daniel@fehrm.se>
2022-09-21 14:26:02 +0200

    added tests for equation lines

--- tests/test_pdf2txt.py
+++ tests/test_pdf2txt.py
@@ -1,5 +1,6 @@
 import contextlib
 import io
+import re
 import unittest
 from unittest.mock import patch
 from tools.pdf2txt import main
@@ -42,4 +43,41 @@ class TestPdf2Txt(unittest.TestCase):
 
         self.run_tests(
             tests, ['pdf2txt.py', '-t', 'html', 'samples/simple1.pdf']
+        )
+
+    def test_equations_html_output(self):
+        def tests(fake_stdout):
+            # Assert that there are three lines between the equations
+            self.assertRegex(
+                fake_stdout.getvalue(),
+                re.compile(
+                    '<span.*border: black 1px solid.*'
+                    '<span.*border: black 1px solid.*'
+                    '<span.*border: black 1px solid.*',
+                    re.DOTALL
+                )
+            )
+
+            # Assert that one of the equations are in the output
+            self.assertRegex(fake_stdout.getvalue(), r'3.*x.*2.*\+ 5')
+
+        self.run_tests(
+            tests, ['pdf2txt.py', '-t', 'html', 'samples/equations.pdf']
+        )
+
+    def test_equations_xml_output(self):
+        def tests(fake_stdout):
+            # Assert that there are three lines between the equations
+            self.assertRegex(
+                fake_stdout.getvalue(),
+                re.compile('<line.*<line.*<line', re.DOTALL)
+            )
+            # Assert that one of the equations are in the output
+            self.assertRegex(
+                fake_stdout.getvalue(),
+                re.compile('3.*x.*2.*\+.*5', re.DOTALL)
+            )
+
+        self.run_tests(
+            tests, ['pdf2txt.py', '-t', 'xml', 'samples/equations.pdf']
         )
